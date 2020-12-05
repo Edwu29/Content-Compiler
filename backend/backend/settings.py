6591d2e43dcee153d2dbff1ad18133c153840928
Edwu29 <edwardwu10@gmail.com>
2020-12-05 14:32:13 -0800

    django secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$8ve!vp@h_$a-6ea77!qxm+oy#kgkareu=+%+m*1tymd5!(3c6'
+load_dotenv()
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-    path('', include('frontend.urls'))
+   #path('', include('frontend.urls'))
 ]
