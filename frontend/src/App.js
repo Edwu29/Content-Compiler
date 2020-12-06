84fa95bb285f881991cd27b3d55de8db700323a8
Ryan Mohta <ryanmohta@gmail.com>
2020-12-06 00:04:17 -0800

    Created CheckboxesGroup class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,12 @@ import SearchIcon from '@material-ui/icons/Search';
 import FilterListIcon from '@material-ui/icons/FilterList';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Popover from '@material-ui/core/Popover';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -51,6 +57,9 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     backgroundColor: red[500],
+  },
+  formControl: {
+    margin: theme.spacing(3),
   },
 }));
 
@@ -99,6 +108,43 @@ function MyPopover() {
       >
         <Typography className={classes.typography}>The content of the Popover.</Typography>
       </Popover>
+    </div>
+  );
+}
+
+function CheckboxesGroup() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const { gilad, jason, antoine } = state;
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Assign responsibility</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
+            label="Gilad Gray"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
+            label="Jason Killian"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
+            label="Antoine Llorca"
+          />
+        </FormGroup>
+      </FormControl>
     </div>
   );
 }
